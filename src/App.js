0a3519ffcdbb1acbf01fe8e19aa9ff813d30d260
Kushal Nagwanshi <kushalnagwanshicloud@gmail.com>
2023-01-13 00:33:16 +0530

    Made the final layout of the sudoku board ( appearance )

--- src/App.js
+++ src/App.js
@@ -1,23 +1,178 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // let Sudoku = [];
+  // let row = [];
+  // for (let i = 0; i < 9; i++) {
+  //   row.push("");
+  // }
+  // for (let i = 0; i < 9; i++) {
+  //   Sudoku.push(row);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="SodokuTable centerdivgrid">
+      <div className="Title center">
+        <h1>Sudoku</h1>
+      </div>
+      <table className="SudokuBox">
+        <tr>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+          <td>
+            <tr>
+              <td className="SudokuCell">1</td>
+              <td className="SudokuCell">2</td>
+              <td className="SudokuCell">3</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">4</td>
+              <td className="SudokuCell">5</td>
+              <td className="SudokuCell">6</td>
+            </tr>
+            <tr>
+              <td className="SudokuCell">7</td>
+              <td className="SudokuCell">8</td>
+              <td className="SudokuCell">9</td>
+            </tr>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
